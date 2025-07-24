1aab90b3b75d44569e1c03a12bf23518fcfdb369
= <amitchen2301@gmail.com>
2025-07-24 11:04:52 +0100

    logs and validation

--- handlers/books.py
+++ handlers/books.py
@@ -1,25 +1,42 @@
 from flask import render_template, request, redirect, url_for, jsonify, flash
 from extensions import db
-from models import Book, Author
+from models import Book, Author ,Review
+import logging
+from validation_logging import errors
+from validation_logging import logging_config
+logger = logging_config.logger
+
 
 def get_books_page():
     books = Book.query.all()
     return render_template('Books.html', books=books)
 
 def add_book_form():
-    author_name = request.form.get('author_input')
+    request_name = request.form.get('author_input')
     book_title = request.form.get('book')
+    author = Author.query.filter_by(author_name=request_name).first()
+    temp_id = str(hash(book_title+author.author_id))
+    try:
+        validation_data = errors.BookInput(
+            book_id= temp_id,
+            book_name= book_title
+        )
+
+    except ValueError as e :
+        logger.error("did not pass validation check")
+        return redirect(url_for('handlers_authors_get_books_page'))
 
     books = Book.query.filter_by(book_name=book_title).all()
     if books:
-        flash(f"Book '{book_title}' already exists!", "error")
+        logger.error("book already exists inside data base")
         return redirect(url_for('handlers_books_get_books_page'))
 
-    author = Author.query.filter_by(author_name=author_name).first()
+    
     if author and book_title:
-        book = Book(book_id=str(hash(book_title+author.author_id)), book_name=book_title, author_foreign=author.author_id)
+        book = Book(book_id=validation_data.book_id, book_name=validation_data.book_name, author_foreign=author.author_id)
         db.session.add(book)
         db.session.commit()
+        logger.info(f"{book_title} was scuessfuly added to the book tables")
         return redirect(url_for('handlers_books_get_books_page'))
 
 
@@ -33,9 +50,22 @@ def list_books():
 
 def create_book():
     data = request.get_json()
-    book = Book(book_id=str(hash(data['book_name']+data['author_foreign'])), book_name=data['book_name'], author_foreign=data['author_foreign'])
+    temp_id = str(hash(data['book_name']+data['author_foreign']))
+    
+    try:
+        validation_data = errors.BookInput(
+            book_id= temp_id,
+            book_name= data['book_name']
+        )
+
+    except ValueError as e :
+        logger.error("did not pass validation check")
+        return redirect(url_for('handlers_authors_get_authors_page'))
+    
+    book = Book(book_id=validation_data.book_id, book_name=data['book_name'], author_foreign=data['author_foreign'])
     db.session.add(book)
     db.session.commit()
+    logger.info(f"{book.book_id} was sucessfully added to the database")
     return jsonify({"book_id": book.book_id, "book_name": book.book_name, "author_foreign": book.author_foreign}), 201
 
 def get_book(book_id):
@@ -45,40 +75,58 @@ def get_book(book_id):
     return {"book_id": book.book_id, "book_name": book.book_name, "author_foreign": book.author_foreign}
 
 def update_book():
-
-    print("Update book called")
     data = request.get_json()
-    
     book_id = data.get("book_id")
     author_foreign = data.get("author_key")
     book_name = data.get("book_name")
 
-    print(f"book_id: {book_id}, author_foreign: {author_foreign}, book_name: {book_name}")
+    try:
+        validation_data = errors.BookInput(
+            book_id= book_id,
+            book_name= book_name
+        )
+
+    except ValueError as e :
+        logger.error("did not pass validation check")
+        return redirect(url_for('handlers_authors_get_authors_page'))
+    
+
     
     author = Author.query.filter_by(author_id=author_foreign).first()
     if not author:
+        logger.error("data could not be found, hence is not edited")
         return {"error": "Author not found"}, 404
     
     book = Book.query.get(book_id)
 
     if not book:
-        return {"error": "Not found"}, 404
+        logger.error("book was not foun within the book database")
+        return 404
 
     book.book_name = book_name
     book.author_foreign = author_foreign
 
 
     db.session.commit()
+    logger.info("book was sucesfully edited and updated")
     return {"book_id": book.book_id, "book_name": book.book_name, "author_foreign": book.author_foreign}
 
 def delete_book():
     get_book_id = request.form.get('data_remove')
-    print(f"get_book_id: {get_book_id}")
     book = Book.query.get(get_book_id)
+
     if not book:
+        logger.error(f"book was not found within the database, {get_book_id}")
         return '', 204
     
+    reviews = Review.query.filter_by(book_id=book.book_id).all()
+    if reviews:
+        for review in reviews:
+            db.session.delete(review)
+            logger.info(f"{review}, was sucessfully removed from the review database")
+            
     db.session.delete(book)
     db.session.commit()
+
 
     return redirect(url_for('handlers_books_get_books_page')), 204 
--- handlers/reviews.py
+++ handlers/reviews.py
@@ -1,6 +1,9 @@
 from flask import render_template, request, jsonify, redirect, url_for
 from extensions import db
 from models import Review, Book
+import logging
+from validation_logging import errors
+logger = errors.logger
 
 def get_reviews_page():
     review_table = Review.query.all()
@@ -14,13 +17,26 @@ def list_reviews():
     ])
 
 def create_review():
-
     review_value = request.form.get('review_value')
     book_id = request.form.get('book_id')
+    temp_id = str(hash(str(review_value)))
 
-    review = Review(review_id=str(hash(str(review_value))), review_value=review_value, book_id=book_id)
+    try:
+        validation_data = errors.ReviewInput(
+            review_id = temp_id,
+            review_value = review_value,
+            book_id = book_id
+        )
+
+    except ValueError as e :
+        logger.error("did not pass validation check")
+        return redirect(url_for('handlers_reviews_list_reviews'))
+    
+    review = Review(review_id=temp_id, review_value=review_value, book_id=book_id)
     db.session.add(review)
     db.session.commit()
+
+    logger.info("reivew was sucesfully added to database")
     return redirect(url_for('handlers_reviews_get_reviews_page')), 201
 
 def get_review(review_id):
@@ -31,20 +47,42 @@ def get_review(review_id):
 
 def update_review(review_id):
     review = Review.query.get(review_id)
-    if not review:
-        return {"error": "Not found"}, 404
     data = request.get_json()
+
+    try:
+        validation_data = errors.ReviewInput(
+            review_id = review_id,
+            review_value =  data['review_value'],
+            book_id = data['update_book_id']
+        )
+
+    except ValueError as e :
+        logger.error("did not pass validation check")
+        return redirect(url_for('handlers_authors_get_reviews_page'))
+    
+    if not review:
+        logger.error("rewview was not found when trying to update")
+        return {"error": "Not found"}, 404
+    
     if 'review_value' in data:
         review.review_value = data['review_value']
+
     if 'update_book_id' in data:
         review.book_id = data['update_book_id']
+
     db.session.commit()
+    logger.info("review was sucessfully updated")
     return jsonify({"redirect": url_for('handlers_reviews_get_reviews_page')})
+
 
 def delete_review(review_id):
     review = Review.query.get(review_id)
     if not review:
+        logger.error("review aws not removed from table due to not existing")
         return '', 204
+    
     db.session.delete(review)
     db.session.commit()
+
+    logger.info("review was sucessfully deleted from the datbase")
     return redirect(url_for('handlers_reviews_get_reviews_page')), 204 
